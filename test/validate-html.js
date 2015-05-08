f2000cb50cf03b9a8449024fcfd28acd03adae1e
Michael Robinson <michael.robinson@kiwirail.co.nz>
2015-05-08 12:21:11 +1200

    Refactored test to be synchronous

--- test/validate-html.js
+++ test/validate-html.js
@@ -1,41 +1,60 @@
 var expect = require('chai').expect,
-  validate = require('html-tag-validator'),
-  fs = require('fs'),
-  path = require('path'),
-  walk = require('walk').walk;
+    validate = require('html-tag-validator'),
+    fs = require('fs'),
+    path = require('path'),
+    walk = require('walk').walkSync;
 
-describe('html validity', function(done) {
-  it('should all be structurally valid', function(done) {
-    this.timeout(20000);
-    var walker = walk(path.join(__dirname, '/../src/'));
+describe('html validity', function() {
+    it('should all be structurally valid', function(done) {
 
-    walker.on('file', function(root, fileStats, walkerNext) {
-      if (!/html$/.test(fileStats.name)) {
-        return walkerNext();
-      }
+        var fileCount = 0,
+            validationCount = 0;
+        var countOptions = {
+            listeners: {
+                file: function(root, fileStats, walkerNext) {
+                    if (!/html$/.test(fileStats.name)) {
+                        return walkerNext();
+                    }
+                    fileCount += 1;
+                    walkerNext();
+                }
+            }
+        };
+        var walker = walk(path.join(__dirname, '/../src/'), countOptions);
 
-      var filePath = path.join(root, fileStats.name);
-      content = fs.readFileSync(filePath, 'utf-8');
-      validate(content, {
-        attributes: {
-          _: {
-            mixed: [
-              /^((ng\-)|(^\[[\S]+\]$)|(^\([\S]+\)$))/
-            ]
-          }
-        }
-      }, function(error) {
-        if (error) {
-          var message = "\n\n" + filePath + "\n\n" + error.message + "\nLine: " + error.line + " Column: " + error.column + "\n\n";
-          expect(error).to.equal(null, message);
-        }
-      });
-      walkerNext();
+        var options = {
+            listeners: {
+                file: function(root, fileStats, walkerNext) {
+                    if (!/html$/.test(fileStats.name)) {
+                        return walkerNext();
+                    }
+
+                    var filePath = path.join(root, fileStats.name);
+                    console.log(filePath);
+                    content = fs.readFileSync(filePath, 'utf-8');
+                    validate(content, {
+                        attributes: {
+                            _: {
+                                mixed: [
+                                    /^((ng\-)|(^\[[\S]+\]$)|(^\([\S]+\)$))/
+                                ]
+                            }
+                        }
+                    }, function(error) {
+                        var message = null;
+                        if (error) {
+                            message = "\n\n" + filePath + "\n\n" + error.message + "\nLine: " + error.line + " Column: " + error.column + "\n\n";
+                        }
+                        expect(error).to.equal(null, message);
+                    });
+                    if (fileCount == validationCount) {
+                        done();
+                    }
+                    walkerNext();
+                }
+            }
+        };
+
+        var walker = walk(path.join(__dirname, '/../src/'), options);
     });
-
-    walker.on('end', function() {
-      done();
-    });
-  });
 });
-
